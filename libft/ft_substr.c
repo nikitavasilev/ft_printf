0e3957d67df8cb71a6e5251714b7400ae57cb72e
Nikita VASILEV <nvasilev@student.42.fr>
2021-09-13 23:05:09 +0200

    faster version + fix leak

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/09 13:46:27 by nvasilev          #+#    #+#             */
-/*   Updated: 2021/09/13 22:44:52 by nvasilev         ###   ########.fr       */
+/*   Updated: 2021/09/13 23:04:21 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,24 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
+	size_t	s_len;
 	size_t	i;
 
 	if (!s)
 		return (0);
-	str = malloc(len + 1);
+	s_len = ft_strlen(s);
+	if (start > s_len)
+		s_len = 0;
+	if (s_len > len)
+		s_len = len;
+	str = malloc(s_len + 1);
 	if (!str)
 		return (0);
 	i = 0;
-	while (s[start + i] && i < len && start < ft_strlen(s))
+	if (s_len)
 	{
-		str[i] = s[start + i];
-		i++;
+		while (s[start] && i < len)
+			str[i++] = s[start++];
 	}
 	str[i] = '\0';
 	return (str);
