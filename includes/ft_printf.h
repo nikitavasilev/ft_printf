245565a518a5c73a6ae2f5f5a199f350bfbf735a
Nikita VASILEV <nvasilev@student.42.fr>
2021-09-13 22:07:46 +0200

    ++ includes

---
+++ includes/ft_printf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/13 21:52:48 by nvasilev          #+#    #+#             */
+/*   Updated: 2021/09/13 22:05:26 by nvasilev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include "../libft/libft.h"
+
+int	ft_printf(const char *, ...);
+
+#endif
