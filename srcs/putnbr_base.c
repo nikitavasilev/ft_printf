87aa016187f6c826b414a2800fd8ae289e6a1672
Nikita VASILEV <vassilev.nikita@gmail.com>
2021-09-20 21:59:56 +0200

    split in 2 ft: signed and u

--- srcs/putnbr_base.c
+++ srcs/putnbr_base.c
@@ -6,22 +6,18 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/20 16:30:01 by nvasilev          #+#    #+#             */
-/*   Updated: 2021/09/20 21:16:25 by nvasilev         ###   ########.fr       */
+/*   Updated: 2021/09/20 21:57:03 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-int	putnbr_base(intmax_t n, const char *base, const char sign, int count)
+int	putnbr_base_s(int64_t n, const char *base, int count)
 {
 	int64_t	len;
 
 	count++;
 	len = ft_strlen(base);
-	if (sign == 'p')
-		n = (uintmax_t)n;
-	else if (len == 16 || sign == 'u')
-		n = (unsigned)n;
 	if (n < 0)
 	{
 		ft_putchar('-');
@@ -29,7 +25,19 @@ int	putnbr_base(intmax_t n, const char *base, const char sign, int count)
 		count++;
 	}
 	if (n >= len)
-		count = putnbr_base(n / len, base, sign, count);
+		count = putnbr_base_s(n / len, base, count);
+	ft_putchar(base[n % len]);
+	return (count);
+}
+
+int	putnbr_base_u(size_t n, const char *base, int count)
+{
+	size_t len;
+
+	count++;
+	len = ft_strlen(base);
+	if (n >= len)
+		count = putnbr_base_u(n / len, base, count);
 	ft_putchar(base[n % len]);
 	return (count);
 }
