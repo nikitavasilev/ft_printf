42a50f95d0b53455276be1bbc50bf6ff61f0e9cd
Nikita VASILEV <vassilev.nikita@gmail.com>
2021-09-20 21:23:37 +0200

    counter fix + bigger type

--- srcs/putnbr_base.c
+++ srcs/putnbr_base.c
@@ -6,31 +6,30 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/20 16:30:01 by nvasilev          #+#    #+#             */
-/*   Updated: 2021/09/20 17:14:16 by nvasilev         ###   ########.fr       */
+/*   Updated: 2021/09/20 21:16:25 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-int	putnbr_base(int64_t n, const char *base, const char sign, int count)
+int	putnbr_base(intmax_t n, const char *base, const char sign, int count)
 {
 	int64_t	len;
 
+	count++;
 	len = ft_strlen(base);
 	if (sign == 'p')
-		n = (uint64_t)n;
+		n = (uintmax_t)n;
 	else if (len == 16 || sign == 'u')
 		n = (unsigned)n;
 	if (n < 0)
 	{
 		ft_putchar('-');
 		n *= -1;
-	}
-	if (n >= len)
-	{
-		count += putnbr_base(n / len, base, sign, count);
 		count++;
 	}
+	if (n >= len)
+		count = putnbr_base(n / len, base, sign, count);
 	ft_putchar(base[n % len]);
 	return (count);
 }
