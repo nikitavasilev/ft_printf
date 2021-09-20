3a84af780468088f1b150928d615aedc3042d9fb
Nikita VASILEV <vassilev.nikita@gmail.com>
2021-09-20 21:21:32 +0200

    bases

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/13 21:52:48 by nvasilev          #+#    #+#             */
-/*   Updated: 2021/09/20 17:18:16 by nvasilev         ###   ########.fr       */
+/*   Updated: 2021/09/20 20:55:42 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,14 @@
 # include <stdint.h>
 # include <limits.h>
 
+# define DEC "0123456789"
+# define HEX_MAJ "0123456789ABCDEF"
+# define HEX_MIN "0123456789abcdef"
+
 int		ft_putchar(int c);
-void	ft_putstr(char *s);
+size_t	ft_putstr(char *s);
 size_t	ft_strlen(const char *s);
-int		putnbr_base(int64_t n, const char *base, const char sign, int count);
+int		putnbr_base(intmax_t n, const char *base, const char sign, int count);
 int		putaddr(const void *p, int count);
 int		format_specifier(const char format, int count, va_list arg);
 int		ft_printf(const char *format, ...);
