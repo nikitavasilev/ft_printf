187ddd31237e26a9df9e6e01f56fa8266e9088ab
Nikita VASILEV <vassilev.nikita@gmail.com>
2021-09-20 21:23:59 +0200

    bigger type

--- srcs/putaddr.c
+++ srcs/putaddr.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/20 16:30:33 by nvasilev          #+#    #+#             */
-/*   Updated: 2021/09/20 16:30:35 by nvasilev         ###   ########.fr       */
+/*   Updated: 2021/09/20 21:19:26 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 int	putaddr(const void *p, int count)
 {
-	uint64_t	addr;
+	uintmax_t	addr;
 
-	addr = (uint64_t)p;
+	addr = (uintmax_t)p;
 	ft_putstr("0x");
-	count += putnbr_base(addr, "0123456789abcdef", 'p', count) + 2;
+	count += putnbr_base(addr, HEX_MIN, 'p', count) + 2;
 	 return (count);
 }
