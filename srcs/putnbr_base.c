572927478dd704ef37d8b727f817af7669b1b621
Nikita VASILEV <vassilev.nikita@gmail.com>
2021-09-20 22:02:29 +0200

    fix norm

--- srcs/putnbr_base.c
+++ srcs/putnbr_base.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/20 16:30:01 by nvasilev          #+#    #+#             */
-/*   Updated: 2021/09/20 21:57:03 by nvasilev         ###   ########.fr       */
+/*   Updated: 2021/09/20 22:02:13 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ int	putnbr_base_s(int64_t n, const char *base, int count)
 
 int	putnbr_base_u(size_t n, const char *base, int count)
 {
-	size_t len;
+	size_t	len;
 
 	count++;
 	len = ft_strlen(base);
