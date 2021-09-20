2141e8b7cd18a7059ff51997215cd611d3ada144
Nikita VASILEV <vassilev.nikita@gmail.com>
2021-09-20 23:34:01 +0200

    OS macros

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/13 21:52:48 by nvasilev          #+#    #+#             */
-/*   Updated: 2021/09/20 21:57:50 by nvasilev         ###   ########.fr       */
+/*   Updated: 2021/09/20 23:30:11 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,20 @@
 # define HEX_MAJ "0123456789ABCDEF"
 # define HEX_MIN "0123456789abcdef"
 
+# ifdef __linux__
+#  define RET_NULL "(nil)"
+# else
+#  define RET_NULL "(null)"
+# endif
+
 int		ft_putchar(int c);
 size_t	ft_putstr(char *s);
 size_t	ft_strlen(const char *s);
 int		putnbr_base_s(int64_t n, const char *base, int count);
 int		putnbr_base_u(size_t n, const char *base, int count);
 int		putaddr(const void *p, int count);
+int		check_null_p(void *p);
+int		check_null_s(char *s);
 int		format_specifier(const char format, int count, va_list arg);
 int		ft_printf(const char *format, ...);
 
