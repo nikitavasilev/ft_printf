325a25cdc5fdb8af194622a23de534eb8672a090
Nikita VASILEV <nvasilev@student.42.fr>
2021-09-23 03:06:15 +0200

    test 0 flag

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/14 22:58:29 by nvasilev          #+#    #+#             */
-/*   Updated: 2021/09/22 22:54:03 by nvasilev         ###   ########.fr       */
+/*   Updated: 2021/09/23 03:03:48 by nvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,68 +17,52 @@ int	main(void)
 	char	*str;
 
 	str = "test";
-	//printf("MINE:\n");
-	//printf("\nRETURN: %d\n\n", ft_printf("Nombre: %d, adresse: %p, percent: %%", 42, &str));
 
-	ft_putstr("TESTER (ft_printf):\n");
+	printf("RET: %d\n", printf("%010i\n", 25987));
+	printf("RET: %d\n", printf("%.2s\n", str));
+	printf("RET: %d\n", ft_printf("%010i\n", 25987));
+/*
+	ft_printf("TESTER (ft_printf):\n");
 	printf("return: %d\n\n", ft_printf(" NULL %s NULL ", NULL));
 	printf("return: %d\n\n", ft_printf(" %p %p ", NULL, NULL));
-	//printf("\nRETURN: %d\n\n", ft_printf("Adresse: %p", &str));
-	//printf("\nRETURN: %d\n\n", ft_printf("M%d", 4243));
-	//printf("REAL ONE:\n");
-	//printf("\nRETURN: %d\n\n", printf("Nombre: %d, adresse: %p, percent: %%", 42, &str));
+
 	printf("\nTESTER (printf):\n");
 	printf("return: %d\n\n", printf(" NULL %s NULL ", NULL));
 	printf("return: %d\n\n", printf(" %p %p ", NULL, NULL));
-	//printf("\nRETURN: %d\n\n", printf("Adresse: %p", &str));
-	//printf("\nRETURN: %d\n\n", printf("M%d", 4243));
 
-	/*
-	printf("\n\nc = %c (printf)\n", str[0]);
-	ft_putstr("c = ");
-	ft_putchar(str[0]);
-	ft_putstr(" (ft_putchar)");
+	ft_printf("\nc = %c (ft_printf)\n", str[0]);
+	printf("c = %c (printf)\n", str[0]);
 
-	printf("\n\ns = %s (printf)\n", str);
-	ft_putstr("s = ");
-	ft_putstr(str);
-	ft_putstr(" (ft_putstr)");
+	ft_printf("\ns = %s (ft_printf)\n", str);
+	printf("s = %s (printf)\n", str);
 
-	printf("\n\np = %p (printf)\n", &str);
-	ft_putstr("p = ");
-	putaddr(&str);
-	ft_putstr(" (ft_putaddr)");
+	ft_printf("\np = %p (ft_printf)\n", &str);
+	printf("p = %p (printf)\n", &str);
 
-	printf("\n\nd = %d (printf)\n", 0000000000101010);
-	ft_putstr("d = ");
-	putnbr_base(0000000000101010, "0123456789", 's');
-	ft_putstr(" (putnbr_base)");
+	printf("\nd = %d (printf)\n", 0000000000101010);
+	ft_printf("d = %d (ft_printf)\n", 0000000000101010);
 
-	printf("\n\ni = %i (printf)\n", 042);
-	ft_putstr("i = ");
-	putnbr_base(042, "0123456789", 's');
-	ft_putstr(" (putnbr_base)");
+	ft_printf("\ni = %i (ft_printf)\n", 042);
+	printf("i = %i (printf)\n", 042);
 
-	printf("\n\nu = %u (printf)\n", -42);
-	ft_putstr("u = ");
-	putnbr_base(-42, "0123456789", 'u');
-	ft_putstr(" (putnbr_base)");
+	ft_printf("\nu = %u (ft_printf)\n", -42);
+	printf("u = %u (printf)\n", -42);
+
+	ft_printf("\nx = %x (ft_printf)\n", 42);
+	printf("x = %x (printf)\n", 42);
+
+	ft_printf("\nX = %X (ft_printf)\n", -42);
+	printf("X = %X (printf)\n", -42);
+
+	ft_printf("\n%% = %% (ft_printf)\n");
+	printf("%% = %% (printf)");
+	printf("\n");
+
+	ft_printf("\nMIX: (ft_printf)");
+	ft_printf("\nRETURN: %d", ft_printf("\nNombre: %d, adresse: %p, percent: %%", 42, &str));
+
+	printf("\n\nMIX: (printf)");
+	printf("\nRETURN: %d", printf("\nNombre: %d, adresse: %p, percent: %%", 42, &str));
 */
-	printf("\n\nx = %x (printf)\n", 42);
-	ft_putstr("x = ");
-	putnbr_base_s(42, "0123456789abcdef", 0);
-	ft_putstr(" (putnbr_base)");
-
-	printf("\n\nX = %X (printf)\n", -42);
-	ft_putstr("X = ");
-	putnbr_base_s(-42, "0123456789ABCDEF", 0);
-	ft_putstr(" (putnbr_base)");
-
-	printf("\n\n%% = %% (printf)\n");
-	ft_putstr("% = ");
-	ft_putchar('%');
-	ft_putstr(" (ft_putchar)");
-
-
 	return (0);
 }
